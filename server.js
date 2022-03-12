d62b7d8a4591b0e6a0413d638f0ec776905c3bbe
Logan Davis <Ldavis96@live.com>
2022-03-12 12:18:30 -0500

    user get and post routes are functioning, need to fix UserSchema.virtuals to align

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.use(require("./routes"));
+app.use(require("./routes/api"));
 
-// mongoose.connect(process.env.MONGODB_URI || "", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network-api",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 // Use this to log mongo queries being executed!
 mongoose.set("debug", true);
