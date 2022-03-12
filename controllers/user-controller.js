93d38ca717047d37ede7e6a53b3869b671eef0d0
Logan Davis <Ldavis96@live.com>
2022-03-12 12:16:04 -0500

    get and post routes for users are functioning minus the UserSchema.virtual

---
+++ controllers/user-controller.js
@@ -0,0 +1,23 @@
+const { User } = require("../models");
+
+const userController = {
+  // get all users
+  getAllUser(req, res) {
+    User.find({})
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(400).json(err));
+  },
+};
+
+module.exports = userController;
